2c3f687e9b38d5a513cd5719b2afeee756529137
lucas <lucas@cenersys.com>
2019-11-23 12:12:38 -0500

    added build folder becasue d-ocean droplet doesnt have enought memory

---
+++ build/precache-manifest.63abf8741e86e410b4eb75ce319176cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca8f84f04f1c2f0fb278eb77b18bb82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e19892eec8211f26f49",
+    "url": "/static/css/main.21b79796.chunk.css"
+  },
+  {
+    "revision": "c4812bbda0fde269b5a0",
+    "url": "/static/js/2.b3089ca2.chunk.js"
+  },
+  {
+    "revision": "7e19892eec8211f26f49",
+    "url": "/static/js/main.88b2d618.chunk.js"
+  },
+  {
+    "revision": "a1218636fae64cf70443",
+    "url": "/static/js/runtime-main.e3971a46.js"
+  }
+]);
